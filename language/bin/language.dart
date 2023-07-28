d30063ecc79886b5bbabe8a4a6e615728529f6df
Jahongirrahmanshikov <jahonggir617@gmail.com>
2023-07-28 10:27:24 +0500

    added  translator package

---
+++ language/bin/language.dart
@@ -0,0 +1,5 @@
+import 'package:language/language.dart' as language;
+
+void main(List<String> arguments) {
+  print('Hello world: ${language.calculate()}!');
+}
---
+++ language/test/language_test.dart
@@ -0,0 +1,8 @@
+import 'package:language/language.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
